1c7fe5f45674080241f6ecaf76da61036b832a6f
Ajib Jawwad <81941882+ajib-jawwad@users.noreply.github.com>
2023-11-14 15:40:35 +0700

    fix verification

--- slack/index.js
+++ slack/index.js
@@ -10,7 +10,7 @@ exports.initSlackClient = () => {
     });
 
     return app.client;
-}
+};
 
 exports.replySlackToChannel = async (client, msg) => {
     const channelID = process.env.CHANNEL_ID;
@@ -23,25 +23,26 @@ exports.replySlackToChannel = async (client, msg) => {
     } catch (err) {
         console.log(JSON.stringify(err));
     }
-}
+};
 
 exports.verifySignature = (req) => {
     const slackSignature = req.headers['x-slack-signature'];
     const requestBody = qs.stringify(req.body,{ format:'RFC1738' });
     const timestamp = req.headers['x-slack-request-timestamp'];
 
-    let sigBasestring = 'v0=' + timestamp + ':' + requestBody;
+    let sigBasestring = 'v0:' + timestamp + ':' + requestBody;
     const slackSigningSecret = process.env.SLACK_SIGNING_SECRET;
 
     let mySignature = 'v0=' + crypto.createHmac('sha256', slackSigningSecret).update(sigBasestring, 'utf8').digest('hex');
-
+    
     return crypto.timingSafeEqual(Buffer.from(mySignature, 'utf8'),Buffer.from(slackSignature, 'utf8'));
-}
+
+};
 
 exports.replyToSlackCommand = (msg, res_type, url) => {
     console.log('sent: ', msg);
     axios.post(url, {
         response_type: res_type == undefined ? "in_channel" : res_type,
         text: msg,
-    })
-}
+    });
+};
